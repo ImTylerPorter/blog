9b6470cbca47fb78d25362f5628ec0846bba6c62
Tyler Porter <hello@cleverladder.com>
2015-10-14 12:55:21 -0500

    Added Post CRUD ability

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+
+	def index
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new post_params
+
+		if @post.save
+			redirect_to @post, notice: 'Hellz yeah, TP! Your Article was succesfully saved!'
+		else
+			render 'new', notice: "Oh nom TP! I was unable to save your post."
+		end
+	end
+
+	# Post found with before_action
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	# Post found with before_action
+	def edit
+	end
+
+	# Post found with before_action
+	def update
+		if @post.update post_params
+			redirect_to @post, notice: "OH YEA! Your Post was succesfully saved!"
+		else
+			render 'edit'
+		end
+	end
+	
+	# Post found with before_action
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
+
+end
