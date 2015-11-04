11d228816703dcd8cff75d5ae9f6b2d40bdbf262
Tyler Porter <hello@cleverladder.com>
2015-11-04 16:57:45 -0600

    Add aws-sdk gem & config to production.rb for amazon s3 storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,15 @@ ActionMailer::Base.smtp_settings = {
   :enable_starttls_auto => true
 }
 
+# Sets paperclip to upload to AMAZON s3 by default
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 
 end
 
