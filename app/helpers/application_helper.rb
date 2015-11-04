ad7c05570787d4d01d52a21120bc69f471920120
Tyler Porter <hello@cleverladder.com>
2015-11-04 15:18:29 -0600

    Style & structure Profile page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,7 +21,7 @@ module ApplicationHelper
 
 
 	def profile_avatar_select(user)
-    return image_tag user.avatar.url(:medium),
+    return image_tag user.avatar.url,
                      id: 'image-preview',
                      class: 'img-responsive img-circle profile-image' if user.avatar.exists?
     								image_tag 'default-avatar.jpg', id: 'image-preview',
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
   
-  has_attached_file :avatar, :styles => { :medium => "300#" }
+  has_attached_file :avatar, :styles => { :medium => "200#" }
 
   validates_presence_of :avatar
    validates_attachment :avatar, :presence => true,
