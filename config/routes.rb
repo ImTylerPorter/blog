7c4c1e05bb3bb6f006bf70b46005cd96036f9a9c
Tyler Porter <hello@cleverladder.com>
2015-11-05 14:54:34 -0600

    Bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -12,12 +12,12 @@ Rails.application.routes.draw do
  get ':user_name', to: 'profiles#show', as: :profile
  get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
  patch ':user_name/edit', to: 'profiles#update', as: :update_profile
-  get 'welcome/index'
 
 
 	authenticated :user do 
 	  	root 'welcome#index', as: "authenticated_root"
 	  end
+	    get 'welcome/index'
 	root 'pages#home'
 
  get '*path' => redirect('/')
