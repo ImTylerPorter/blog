d7a42798787a71e50a6e9f44d2d46c4490b3b16d
Tyler Porter <hello@cleverladder.com>
2015-11-09 18:17:42 -0600

    Learning here, Adjust meta  settings

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,7 @@ class WelcomeController < ApplicationController
 		@posts = Post.all.limit(3).order("created_at desc")
 		@projects = Project.all.limit(3).order("created_at desc")
 		@tyler = User.first
+
+		prepare_meta_tags(title: "Tyler Porter will be coming soon!")
 	end
 end
